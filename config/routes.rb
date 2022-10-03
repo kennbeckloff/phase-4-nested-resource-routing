e192291a43c66b0fedb73d3eb95119ae0d5f4102
kennbeckloff <kennbeckloff688@gmail.com>
2022-10-03 19:19:31 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   resources :dog_houses, only: [:show]
   resources :reviews, only: [:show, :index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  def reviews_index
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  endr
 end
